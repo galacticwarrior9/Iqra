fdaa34bc21e0740c9ea8b1fee4b0326d893f96cc
galacticwarrior9 <zareef1@yahoo.com>
2024-05-31 23:24:47 +0100

    Only automatically disconnect the bot after 24 hours of inactivity in its voice channel

--- src/main/kotlin/com/gmail/galacticwarrior9/listeners/VoiceChannelListener.kt
+++ src/main/kotlin/com/gmail/galacticwarrior9/listeners/VoiceChannelListener.kt
@@ -5,8 +5,29 @@ import net.dv8tion.jda.api.entities.Guild
 import net.dv8tion.jda.api.entities.channel.middleman.AudioChannel
 import net.dv8tion.jda.api.events.guild.voice.GuildVoiceUpdateEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
+import net.jodah.expiringmap.ExpirationPolicy
+import net.jodah.expiringmap.ExpiringMap
+import java.util.concurrent.TimeUnit
 
 class VoiceChannelListener : ListenerAdapter() {
+    companion object {
+        private val channelLastInteractionMap: ExpiringMap<Guild, AudioChannel> = ExpiringMap.builder()
+            .expirationPolicy(ExpirationPolicy.ACCESSED)
+            .expiration(1, TimeUnit.DAYS)
+            .expirationListener { guild: Guild, channel: AudioChannel ->
+                if (channel.members.size <= 1) {
+                    guild.audioManager.closeAudioConnection()
+                    AudioManager.removeGuildHandler(guild)
+                } else {
+                    updateLastInteractionTime(guild, channel)
+                }
+            }
+            .build()
+
+        fun updateLastInteractionTime(guild: Guild, channel: AudioChannel) {
+            channelLastInteractionMap[guild] = channel
+        }
+    }
 
     override fun onGuildVoiceUpdate(event: GuildVoiceUpdateEvent) {
         // Bot left voice chat - remove guild handler
@@ -15,18 +36,28 @@ class VoiceChannelListener : ListenerAdapter() {
         }
 
         // Someone has left a voice channel - check if the bot is now alone. If so, disconnect.
-        if (event.channelLeft != null) {
-            processBotMove(event.guild, event.channelLeft!!, event.channelJoined)
-        }
+        processBotMove(event.guild, event.channelLeft, event.channelJoined)
     }
 
-    private fun processBotMove(guild: Guild, channelLeft: AudioChannel, channelJoined: AudioChannel?) {
+    private fun processBotMove(guild: Guild, channelLeft: AudioChannel?, channelJoined: AudioChannel?) {
         val botChannel = guild.audioManager.connectedChannel ?: return
+        if (botChannel !== channelLeft && botChannel !== channelJoined) {
+            return
+        }
 
-        if (botChannel === channelLeft && channelLeft.members.size == 1
-            || botChannel === channelJoined && channelJoined.members.size == 1) {
-            guild.audioManager.closeAudioConnection()
-            AudioManager.removeGuildHandler(guild)
+        /* Someone has joined/left the bot's voice channel or moved the bot.
+           Refresh cache, so it doesn't automatically leave. */
+        updateLastInteractionTime(guild, botChannel)
+
+        val guildAudioManager = AudioManager.getGuildHandler(guild)
+        if (guildAudioManager.player.playingTrack !== null) {
+            // Pause if nobody is in the channel, otherwise resume.
+            if (botChannel === channelLeft && channelLeft.members.size == 1
+                || botChannel === channelJoined && channelJoined.members.size == 1) {
+                guildAudioManager.player.isPaused = true
+            } else {
+                guildAudioManager.player.isPaused = false
+            }
         }
     }
 }
